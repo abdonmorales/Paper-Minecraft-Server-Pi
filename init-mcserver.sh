cc6ad20ff452041c5a32e1a723393d1b8a20f561
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2021-06-09 18:19:22 -0500

    Adding new features to further automate the process

---
+++ init-mcserver.sh
@@ -0,0 +1,21 @@
+wget https://github.com/abdonmorales/Minecraft-Server/raw/vanilla-main/minecraft_server.1.17.jar
+
+echo "Finish downloading Server JAR (1.17) for MC SERVER v1.3"
+
+echo Making directory to store the JAR
+
+cd ~
+mkdir Minecraft
+cd Minecraft
+mkdir Servers
+cd Servers
+mkdir 1.17-vanilla
+cd ~/Minecraft-Server-1.3.2-stable/
+mv minecraft_server* ~/Minecraft/Servers/1.17-vanilla/
+
+echo "Finished creating and moving the JAR to it's new location"
+echo "~/Minecraft/Servers/1.17-vanilla/"
+
+echo "Now executing JAR file"
+
+exec java -Xmx1G -Xms1G -jar minecraft_server.1.17.jar
